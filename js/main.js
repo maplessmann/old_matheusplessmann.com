4db4c14b4ec3a409c054fe854f9d52614ffe8baf
Matheus Plessmann <maplessmann@gmail.com>
2017-06-19 02:14:09 -0300

    Alterando as animações

--- js/main.js
+++ js/main.js
@@ -5,12 +5,26 @@
     $body = $('body');
 
     $window.on('load', function() {
-        window.setTimeout(function() {
-            $body.addClass('loaded');
-        }, 250);
+
+        // window.setTimeout(function() {
+        //     $body.addClass('loaded');
+        // }, 250);
+
+        /* Carrega as animações uma após a outra */
+        $('.animate-onload').each(function(i) {
+            var that = $(this);
+
+            setTimeout(function() {
+                that.addClass('is-visible');
+            }, 350 * i);
+        });
+
     });
 
 })();
+
+
+
 
 /* Adiciona ou remove a classe 'active' da label, conforme o estado do input */
 $('.input-field input, .input-field textarea').focusout(function() {
