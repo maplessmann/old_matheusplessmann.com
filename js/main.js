bcd40000cf88128aa062e017f754ed5025b13aca
Matheus Plessmann <maplessmann@gmail.com>
2017-06-22 00:23:41 -0300

    Adicionando scroll suave à navegação

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-/* Adiciona a classe 'loaded' após a página ter sido carregada, para iniciar as animações */
+/*
+ * Adiciona a classe 'loaded' após a página ter sido carregada, para iniciar as animações
+ */
 (function() {
-    /*
-    $body.addClass('is-loading');
-
-    $window.on('load', function() {
-        window.setTimeout(function() {
-            $body.removeClass('is-loading');
-        }, 250);
-    });
-    */
 
     var $window = $(window),
     $body = $('body');
@@ -16,20 +9,10 @@
     $window.on('load', function() {
         window.setTimeout(function() {
             $body.addClass('loaded');
-        }, 250);
+        }, 100);
     });
 
 })();
-
-/* Carrega as animações uma após a outra */
-// $('.has-animation').each(function(i) {
-//     var that = $(this);
-//
-//     setTimeout(function() {
-//         that.addClass('is-visible');
-//         console.log(200 * i);
-//     }, 850 * i);
-// });
 
 
 /*
@@ -53,7 +36,10 @@ on('load', function() {
 
 */
 
-/* Adiciona ou remove a classe 'active' da label, conforme o estado do input */
+
+/*
+ * Adiciona ou remove a classe 'active' da label, conforme o estado do input
+ */
 $('.input-field input, .input-field textarea').focusout(function() {
     var thisElem = $(this);
     var textValue = thisElem.val();
@@ -62,5 +48,22 @@ $('.input-field input, .input-field textarea').focusout(function() {
         thisElem.removeClass('active');
     } else {
         thisElem.addClass('active');
+    }
+});
+
+
+/*
+ * Smooth Scroll
+ */
+$('a[href*="#"]:not([href="#"])').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 700);
+            return false;
+        }
     }
 });
