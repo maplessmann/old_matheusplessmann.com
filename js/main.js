1e02d70d4975eb5e2322d418c31bb3a729458cab
Matheus Plessmann <maplessmann@gmail.com>
2017-08-25 21:27:53 -0300

    Atualizando funções com ES2015

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@
  * Adiciona a classe 'loaded' na tag body após a página ter sido carregada, para iniciar as animações
  */
 (function() {
-    var body = document.querySelector('body');
+    const body = document.querySelector('body');
 
     window.addEventListener('load', function() {
-        setTimeout(function() {
-            body.classList.add('loaded');
-        }, 100);
+        setTimeout(() => body.classList.add('loaded'), 100);
     });
 })();
 
@@ -16,11 +14,11 @@
  * Adiciona ou remove a classe 'active' da label, conforme o estado do input
  */
 (function() {
-    var inputField = document.querySelectorAll('.input-field');
+    const inputs = document.querySelectorAll('.form-contact input:not([type="submit"]), .form-contact textarea');
 
-    for(i=0; i<inputField.length; i++) {
-        inputField[i].querySelector('input, textarea').addEventListener('focusout', function() {
-            var thisLabel = this.parentNode.querySelector('label');
+    inputs.forEach(input => {
+        input.addEventListener('focusout', function() {
+            const thisLabel = this.parentNode.querySelector('label');
 
             if(this.value.length > 0) {
                 thisLabel.classList.add('active');
@@ -28,7 +26,7 @@
                 thisLabel.classList.remove('active');
             }
         });
-    }
+    });
 })();
 
 
@@ -36,20 +34,20 @@
  * Navega suavemente até o elemento de id referenciado no href de um link interno, quando o mesmo for clicado
  */
 (function() {
-    var link = document.querySelectorAll('a[href*="#"]:not([href="#"])');
+    const links = document.querySelectorAll('a[href*="#"]:not([href="#"])');
 
-    for(i=0; i<link.length; i++) {
-        link[i].addEventListener('click', function(event) {
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
             event.preventDefault();
 
-            var alvo = this.getAttribute('href'),
-                alvoDistancia = getOffset(document.querySelector(alvo));
+            const alvo = this.getAttribute('href'),
+                  alvoDistancia = getOffset(document.querySelector(alvo));
 
             $('body, html').animate({
                 scrollTop: alvoDistancia
             }, 500);
         });
-    }
+    });
 })();
 
 function getOffset(el) {
@@ -57,25 +55,3 @@ function getOffset(el) {
 
     return box.top + window.pageYOffset - document.documentElement.clientTop;
 }
-
-
-
-
-
-
-
-
-
-
-// $('a[href*="#"]:not([href="#"])').on('click', function() {
-//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//         if (target.length) {
-//             $('html, body').animate({
-//                 scrollTop: target.offset().top
-//             }, 500);
-//             return false;
-//         }
-//     }
-// });
